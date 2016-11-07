f06294039244e4e22e102cf6c84f13092357e605
李奕辰深圳 <liyichen@bogon>
2016-11-07 17:42:02 +0800

    项目初始化

---
+++ TestDemo/TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by 李奕辰 on 2016/11/7.
+//  Copyright © 2016年 李奕辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by 李奕辰 on 2016/11/7.
+//  Copyright © 2016年 李奕辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
